18b74042910760ee83e769c747f4a5954ffa318e
Rahul Maurya <78677484+k26rahul@users.noreply.github.com>
2024-01-22 14:05:16 +0530

    Vanilla to react in progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
-import { useState } from 'react';
-import GameGrid from './components/GameGrid';
+import { useState, useEffect, createContext } from 'react';
+import GameGrid from './components/GameGrid/GameGrid';
+import TicTacToeGame from './lib/TicTacToeGame/TicTacToeGame';
 
 function App() {
+  const ticTacToeGame = new TicTacToeGame(state => {
+    console.log('[App] [ticTacToeGame:onGameStateChange]', state);
+  });
+
+  const [gameState, setGameState] = useState(ticTacToeGame.state);
+  const GameStateContext = createContext(gameState);
+
   return (
     <>
       <h1>Tic-Tac-Toe-React 👋</h1>
-      <GameGrid></GameGrid>
+      <GameGrid
+        board={gameState.board}
+        handleCellClick={cellId => {
+          console.log(cellId);
+        }}
+      />
     </>
   );
 }
