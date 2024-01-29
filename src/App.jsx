c7dcd3cf1664b7e4ef9a15daca1738c838dd4ff6
Rahul Maurya <78677484+k26rahul@users.noreply.github.com>
2024-01-29 13:43:08 +0530

    Still working on switching to react

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
 
   return (
     <>
-      <h1>Tic-Tac-Toe-React 👋</h1>
+      <h1>Tic-Tac-Toe-React</h1>
       <GameGrid
         board={gameState.board}
         handleCellClick={cellId => {
---
+++ src/components/GameContainer.jsx
@@ -0,0 +1,1 @@
+import GameGrid from './GameGrid/GameGrid';
